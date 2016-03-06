08eeeba10ac68aa59f7bea0ee6d6fb11f63beed9
Matt Robinson <matt@technicalartisan.com>
2016-03-05 21:00:21 -0500

    fixed error in package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='elemental-backend',
     version='0.1.0dev',
-    packages=['elemental-backend'],
+    packages=['elemental_backend'],
     license='Mozilla Public License (MPL) version 2.0',
     long_description=open('README.md').read()
 )
