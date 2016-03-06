cfa8f87538cbd079f54317f5e973b46ab7791551
Matt Robinson <matt@technicalartisan.com>
2016-03-05 20:44:40 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils import setup
+
+
+setup(
+    name='elemental-backend',
+    version='0.1.0dev',
+    packages=['elemental-backend'],
+    license='Mozilla Public License (MPL) version 2.0',
+    long_description=open('README.md').read()
+)
