088036818aa6b174ecc4a76bac30f09b0b66d0a0
Matt Robinson <matt@technicalartisan.com>
2016-04-10 12:53:18 -0400

    setup, doc config cleanup

--- setup.py
+++ setup.py
@@ -1,40 +1,79 @@
-from setuptools import setup
-from os import path
+"""
+A data layer for the Elemental CMS.
+"""
+
+import os
+import sys
+from setuptools import setup, find_packages
 
 
-here = path.abspath(path.dirname(__file__))
+if sys.version_info <= (3, 5):
+    raise ValueError('This package requires Python 3.5 or above')
 
 
-with open(path.join(here, 'README.md'), 'r') as f:
-    long_description = f.read()
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-setup(
-    name='elemental-backend',
-    version='0.2.0dev1',
-    description='Data layer for the Elemental CMS.',
-    long_description=long_description,
-    url='https://github.com/artPlusPlus/elemental-backend',
-    author='Matt Robinson',
-    author_email='matt@technicalartisan.com',
-    license='Mozilla Public License (MPL) version 2.0',
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Intended Audience :: Developers',
-        'Environment :: Web Environment'
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    keywords='elemental cms backend',
-    packages=[
-        'elemental_backend'
-    ],
-    install_requires=[],
-    extras_require={
-        'tests': ['py.test'],
-        'docs': ['sphinx>=1.3.0']
-    }
-)
+with open(os.path.join(HERE, 'README.md'), 'r') as f:
+    __long_description__ = f.read()
+
+
+__project__      = 'elemental-backend'
+__version__      = '0.2'
+__release__      = '0.2.0dev1'
+__author__       = 'Matt Robinson'
+__author_email__ = 'matt@technicalartisan.com'
+__url__          = 'https://github.com/artPlusPlus/elemental-backend'
+__platforms__    = 'ALL'
+__classifiers__ = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+    'Intended Audience :: Developers',
+    'Environment :: Web Environment'
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+__keywords__ = [
+    'elemental',
+    'cms',
+    'backend'
+]
+__requires__ = [
+]
+__extra_requires__ = {
+    'test': ['pytest'],
+    'doc': ['sphinx>=1.3.0']
+}
+__entry_points__ = {
+}
+__license__ = [
+    c.rsplit('::', 1)[1].strip()
+    for c in __classifiers__
+    if c.startswith('License ::')
+][0]
+
+
+def main():
+    setup(
+        name                = __project__,
+        version             = __version__,
+        description         = __doc__,
+        long_description    = __long_description__,
+        classifiers         = __classifiers__,
+        author              = __author__,
+        author_email        = __author_email__,
+        url                 = __url__,
+        license             = __license__,
+        keywords            = __keywords__,
+        packages            = find_packages(),
+        platforms           = __platforms__,
+        install_requires    = __requires__,
+        extras_require      = __extra_requires__,
+        entry_points        = __entry_points__
+    )
+
+
+if __name__ == '__main__':
+    main()
