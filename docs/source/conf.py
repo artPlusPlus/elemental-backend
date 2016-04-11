f2fed547522806acc18f088a2001f4ffd9039fb7
Matt Robinson <matt@technicalartisan.com>
2016-04-10 23:08:33 -0400

    docs config inserts project root into sys.path
    this will hopefully fix the setup not found error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,8 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(1, os.path.abspath(os.path.join(sys.path[0], '..')))
 
 import setup as _setup
 
